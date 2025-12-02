621b70bc5604cd8af00f06359e4249eb7864dd74
eduardozbu-ddripp <eduardozbu@gmail.com>
2025-12-02 10:50:45 -0300

    Create server_card_generator.js

---
+++ server_card_generator.js
@@ -0,0 +1,17 @@
+{
+  "name": "ddripp-card-server",
+  "version": "1.0.0",
+  "description": "Gerador de Cards e Capas ddripp",
+  "main": "server_card_generator.js",
+  "scripts": {
+    "start": "node server_card_generator.js"
+  },
+  "dependencies": {
+    "express": "^4.18.2",
+    "canvas": "^2.11.2",
+    "node-fetch": "^2.6.7"
+  },
+  "engines": {
+    "node": ">=16.0.0"
+  }
+}
